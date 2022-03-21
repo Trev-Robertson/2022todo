eb8f5d899966345022e0f6273f0b6397465a27c0
Trevahr R <trevahrr@ncyber.com>
2022-03-21 06:14:00 -0400

    refactored, removed some comments

--- js/listManagerReducer.js
+++ js/listManagerReducer.js
@@ -1,8 +1,5 @@
 export const listManagerReducer = (state = [], action) => {
 
-    if(!action.payload){
-      return state
-    }
         switch (action.type) {
         case 'CREATE':
             return [...state, action.payload] 
@@ -12,11 +9,11 @@ export const listManagerReducer = (state = [], action) => {
            
            return item.id === action.payload.id ? {...item, ...action.payload} : item
           })
-            console.log('UPDATED THING', updatedState3)
+            
         return updatedState3
 
         case 'DELETE':
-          console.log('ACTIONS', action.payload, state)
+          
           const updatedStateDelete = [...state].filter(item => {
             return item.id !== action.payload.id
            })
